32446c57bb14ee7763a0f4ee5701e6b1fcacf0c0
Patrik Dudits <patrik.dudits@payara.fish>
2020-02-19 16:01:49 +0100

    CLOUD-38: Fix condition when namespace has no labels

--- deployer/deployment-controller/src/main/java/fish/payara/cloud/deployer/kubernetes/DirectProvisioner.java
+++ deployer/deployment-controller/src/main/java/fish/payara/cloud/deployer/kubernetes/DirectProvisioner.java
@@ -190,9 +190,10 @@ class DirectProvisioner implements Provisioner {
     public List<Namespace> getNamespaces() {
         List<Namespace> namespacesList = new ArrayList<>();
         for (var namespace: client.namespaces().list().getItems()) {
-            if ("payara-cloud".equals(namespace.getMetadata().getLabels().get("app.kubernetes.io/managed-by"))) {
-                var project = namespace.getMetadata().getLabels().get("app.kubernetes.io/name");
-                var stage = namespace.getMetadata().getLabels().get("app.kubernetes.io/part-of");;
+            var labels = namespace.getMetadata().getLabels();
+            if (labels != null && "payara-cloud".equals(labels.get("app.kubernetes.io/managed-by"))) {
+                var project = labels.get("app.kubernetes.io/name");
+                var stage = labels.get("app.kubernetes.io/part-of");;
                 namespacesList.add(new Namespace(project, stage));
             }
         }
