9202a73b66865eb88322e1963db5d1c1284cee7f
Patrik Dudits <patrik.dudits@payara.fish>
2020-03-27 12:19:24 +0100

    Use jUnit 5 in Launcher
    Arquillian support for jUnit5 is bit flaky, therefore controller will
    remain at 4 for now.

--- instance/launcher/src/test/java/fish/payara/cloud/instance/tasks/DeserializationTest.java
+++ instance/launcher/src/test/java/fish/payara/cloud/instance/tasks/DeserializationTest.java
@@ -38,11 +38,12 @@
 
 package fish.payara.cloud.instance.tasks;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeserializationTest {
 
