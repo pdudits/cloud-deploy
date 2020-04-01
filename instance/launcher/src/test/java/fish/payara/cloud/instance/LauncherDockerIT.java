6b06efb553b0e3d2fc66a796a3924978e68814e1
Patrik Dudits <patrik.dudits@payara.fish>
2020-04-01 17:57:31 +0200

    CLOUD-62: Add TestContainer test for Cloud instance image

---
+++ instance/launcher/src/test/java/fish/payara/cloud/instance/LauncherDockerIT.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2020 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common Development
+ *  and Distribution License("CDDL") (collectively, the "License").  You
+ *  may not use this file except in compliance with the License.  You can
+ *  obtain a copy of the License at
+ *  https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *  See the License for the specific
+ *  language governing permissions and limitations under the License.
+ *
+ *  When distributing the software, include this License Header Notice in each
+ *  file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *  GPL Classpath Exception:
+ *  The Payara Foundation designates this particular file as subject to the "Classpath"
+ *  exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *  file that accompanied this code.
+ *
+ *  Modifications:
+ *  If applicable, add the following below the License Header, with the fields
+ *  enclosed by brackets [] replaced by your own identifying information:
+ *  "Portions Copyright [year] [name of copyright owner]"
+ *
+ *  Contributor(s):
+ *  If you wish your version of this file to be governed by only the CDDL or
+ *  only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *  elects to include this software in this distribution under the [CDDL or GPL
+ *  Version 2] license."  If you don't indicate a single choice of license, a
+ *  recipient has the option to distribute your version of this file under
+ *  either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *  its licensees as provided above.  However, if you add GPL Version 2 code
+ *  and therefore, elected the GPL Version 2 license, then the option applies
+ *  only if the new code is made subject to such option by the copyright
+ *  holder.
+ */
+
+package fish.payara.cloud.instance;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+public class LauncherDockerIT {
+    @Container
+    public GenericContainer instance = new GenericContainer<>("payara/cloud-instance")
+            .withExposedPorts(8080)
+            .withCopyFileToContainer(MountableFile.forClasspathResource("/docker/"), "/config/")
+            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("payara-micro")))
+            .withCommand("/config") // does it get replaced with /bin/sh here?
+            .waitingFor(Wait.forHttp("/health"));
+
+    @Test
+    public void indexAccessible() throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+        var indexHtml = client.send(HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:"+instance.getMappedPort(8080))).build(),
+                HttpResponse.BodyHandlers.ofString());
+        System.out.println(indexHtml.body());
+        assertTrue(() -> indexHtml.body().contains("doesn't work properly without JavaScript enabled"));
+    }
+}
